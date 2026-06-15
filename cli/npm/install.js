ee314fcddb21bce59da3f8873206a15a81982b82
balathanusan-bc <balathanusan@blackcode.ch>
2026-06-15 17:09:01 +0200

    chore: bump CLI npm package to v1.0.1

--- cli/npm/install.js
+++ cli/npm/install.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
 
-const VERSION = '1.0.0'
+const VERSION = '1.0.1'
 const REPO = 'blackcode-switzerland/bc-issues'
 const BIN_DIR = path.join(__dirname, 'bin')
 
